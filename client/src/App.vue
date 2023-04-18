05afae2e1fe24057f731fdf3207962fdd18f098a
BC <94580108+BC1337@users.noreply.github.com>
2023-04-18 13:54:15 -0500

    Feat: updated navbar to make it responsive

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <nav>
-    <div class="nav-right">
+    <div class="nav-left">
       <router-link to="/">Home</router-link> 
     </div>
-    <div class="nav-left">
+    <div class="nav-right">
       <router-link to="/signup">Sign up</router-link>
       <router-link to="/login">Log in</router-link>
     </div>
@@ -20,12 +20,15 @@
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
-  padding: 30px;
+  height: 70px;
+  margin-bottom: 55px;
+  background: #EEE;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
-
-
 nav a {
   font-weight: bold;
   color: #2c3e50;
@@ -34,4 +37,18 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+.nav-right {
+  width: 100%;
+  display: flex;
+  margin-left: auto; 
+  justify-content: space-evenly;
+}
+
+.nav-left {
+  display: flex;
+  width: 100%;
+  min-width: 50%;
+  margin-left: 5vw;
+}
+
 </style>
